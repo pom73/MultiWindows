1c68987cc97f843bcd9a08f2157de9971d51aac3
pom73 <opomarez@hotmail.com>
2024-07-25 18:40:59 +0200

    Initial Commit

---
+++ MultiWindows/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MultiWindows
+//
+//  Created by Olivier Pomarez on 2024-07-25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
